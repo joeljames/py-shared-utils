c391a268dec1a2b0de525bb7167fd626a1bb87e7
Joel James <joel.james@localmed.com>
2016-09-21 16:18:01 -0500

    Bump version: 0.0.0 → 0.1.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='pysharedutils',
-    version='0.0.0',
+    version='0.1.0',
     description='Some convenient python utils.',
     author='Joel James',
     author_email='joeljames1985@gmail.com',
