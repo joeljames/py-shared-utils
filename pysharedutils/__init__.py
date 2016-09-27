35058f612d30758883fea46f5dcadf9bb264357b
Joel James <joel.james@localmed.com>
2016-09-27 17:11:35 -0500

    Bump version: 0.2.0 → 0.3.0

--- pysharedutils/__init__.py
+++ pysharedutils/__init__.py
@@ -9,7 +9,7 @@ from pysharedutils.lists import *  # NOQA
 from pysharedutils.strings import *  # NOQA
 
 
-__version__ = '0.2.0'
+__version__ = '0.3.0'
 __version_info__ = tuple(__version__.split('.'))
 __short_version__ = __version__
 
